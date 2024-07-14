2eb75e1db230b7f18aba234d1f4d5e4f6322a0a9
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-14 20:46:19 +0000

    rr

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
 
   def show
     the_id = params.fetch("the_id")
+    
     matching_records = Movie.where({ :id => the_id})
 
     @the_movie = matching_records.at(0)
